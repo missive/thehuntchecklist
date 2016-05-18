51b8027031671876ca918c16d482f22f4fe0f265
Philippe-Antoine Lehoux <plehoux@gmail.com>
2016-05-18 08:33:58 -0400

    Add thumbnail upload logic

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,11 +4,21 @@
       var california, tomorrow;
       tomorrow = moment().add('days', 1).format("YYYY-MM-DD");
       california = moment.tz(tomorrow + " 00:01", "America/Los_Angeles");
-      return $("#time-to-post").html(california.clone().tz($('#timezone').val()).format('ha z'));
+      return $("#time-to-post").html(california.clone().tz($('#timezone').val()).format('LT'));
     });
     if (typeof Intl.DateTimeFormat().resolvedOptions().timeZone !== 'undefined') {
-      return $('#timezone').val(Intl.DateTimeFormat().resolvedOptions().timeZone).trigger("change");
+      $('#timezone').val(Intl.DateTimeFormat().resolvedOptions().timeZone).trigger("change");
     }
+    return $("#thumbnail-input").change(function() {
+      var reader;
+      if (this.files && this.files[0]) {
+        reader = new FileReader();
+        reader.onload = function(e) {
+          return $('#thumbnail').attr('style', "background-image: url('" + e.target.result + "')");
+        };
+        return reader.readAsDataURL(this.files[0]);
+      }
+    });
   });
 
 }).call(this);
--- assets/javascripts/application.js.coffee
+++ assets/javascripts/application.js.coffee
@@ -4,9 +4,18 @@ $(document).ready(->
     tomorrow   = moment().add('days', 1).format("YYYY-MM-DD")
     california = moment.tz("#{tomorrow} 00:01", "America/Los_Angeles")
     $("#time-to-post").html(
-      california.clone().tz($('#timezone').val()).format('ha z')
+      california.clone().tz($('#timezone').val()).format('LT')
     )
   )
   unless typeof Intl.DateTimeFormat().resolvedOptions().timeZone is 'undefined'
     $('#timezone').val(Intl.DateTimeFormat().resolvedOptions().timeZone).trigger("change")
+
+  # Thumbnail
+  $("#thumbnail-input").change(->
+    if (this.files && this.files[0])
+      reader = new FileReader()
+      reader.onload = (e)->
+        $('#thumbnail').attr('style', "background-image: url('#{e.target.result}')")
+      reader.readAsDataURL(this.files[0]);
+  )
 )
